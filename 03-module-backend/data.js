e49f0a37788d4446ea147c3a7f9facd2c887fc90
handreygalon <handrey.eg@gmail.com>
2020-07-26 10:07:12 -0300

    # Exporting and importing javascript

---
+++ 03-module-backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "EhnXaybirdA",
+        title: "Criando um projeto do zero para seu portfólio",
+        duration: "44:39",
+        price: "Free"
+    },
+    {
+        id: "0mYq5LrQN1s",
+        title: "TypeScript, o início, de forma prática",
+        duration: "01:10:24",
+        price: "Free"
+    },
+    {
+        id: "GOB-lawExXc",
+        title: "React Hooks com TypeScript",
+        duration: "48:18",
+        price: "Free"
+    },
+    {
+        id: "dlCUvYOlnDo",
+        title: "Usando Serverless, Jest e Node.js para corrigir desafios",
+        duration: "55:52",
+        price: "Free"
+    },
+    {
+        id: "P65RJTTqkN4",
+        title: "Formulários no ReactJS & React Native com Unform",
+        duration: "47:08",
+        price: "Free"
+    },
+    {
+        id: "Fbu7z5dXcRs",
+        title: "SQL no Node.js com Sequelize",
+        duration: "01:40:21",
+        price: "Free"
+    }
+]
--- 03-module-backend/server.js
+++ 03-module-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjuncks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
